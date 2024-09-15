1954aaa4a64353c166d6c03512b248097bafa5b7
ddsteed <ddsteed@gmail.com>
2024-09-15 18:51:57 +0800

    update on Sun Sep 15 18:51:57 CST 2024

--- js/main.321365139cbff31987e2ac035f9b8cee3985d20453d13431bfd4e9dd41ff1921.js
+++ js/main.321365139cbff31987e2ac035f9b8cee3985d20453d13431bfd4e9dd41ff1921.js
@@ -109,37 +109,10 @@
   };
   var initHeaderAnchor_default = initHeaderAnchor;
 
-  // ns-hugo:/Users/fengh/Documents/RDS/BLOG/hugo/themes/jane/assets/js/initFootnoteTooltip.js
-  var initFootnoteTooltip = () => {
-    $(".footnote-ref").each(function() {
-      var id = $(this).attr("href").substr(1), footnote = $(document.getElementById(id)).clone(), outer_wrapper = $("<span>", { "class": "fn-content" }), inner_wrapper = $("<span>", { "class": "fn-text" });
-      footnote.find(".footnote-return").remove();
-      $(this).append(outer_wrapper.append(inner_wrapper.html(footnote.html())));
-    });
-    var position = function() {
-      var content = $(".fn-content").removeAttr("style");
-      if ($(window).width() < 640)
-        content.css("width", $(window).width() / 2);
-      else
-        content.css("width", 340);
-      content.each(function() {
-        var width = $(this).children(".fn-text").outerWidth();
-        $(this).css({
-          "width": width,
-          "margin-left": width / -2
-        });
-      });
-    };
-    position();
-    $(window).resize(position());
-  };
-  var initFootnoteTooltip_default = initFootnoteTooltip;
-
   // <stdin>
   $(document).ready(function() {
     initMobileNavbar_default();
     initToc_default();
     initHeaderAnchor_default();
-    initFootnoteTooltip_default();
   });
 })();
